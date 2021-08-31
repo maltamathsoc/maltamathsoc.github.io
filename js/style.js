63b29bf34db058c61e592374231a6c87ee209642
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2021-08-31 12:40:39 +0200

    Amende openTree() to set .sub_opt height dynamically

--- js/style.js
+++ js/style.js
@@ -15,41 +15,14 @@ function openTree(chevron) {
     chevron.parentElement.parentElement.querySelector(".sub_opt").style.height = "0px";
   } else {
     chevron.style.transform = "rotate(-90deg)";
-    chevron.parentElement.parentElement.querySelector(".sub_opt").style.height = "300px";
+    chevron.parentElement.parentElement.querySelector(".sub_opt").style.height =
+      String(
+        chevron.parentElement.parentElement
+          .getElementsByClassName("sub_opt")[0]
+          .getElementsByTagName("a")[0]
+          .getBoundingClientRect().height *
+          document.getElementsByClassName("sub_opt")[0].getElementsByTagName("a").length +
+          3
+      ) + "px";
   }
 }
-
-/* function openNav(ext, grad_offset, mult) */
-
-/* var mms_logo = document.getElementById("mms_logo"); */
-  /* var side_nav = document.getElementById("side_nav"); */
-  /* var main = document.getElementById("main"); */
-  /* var close_container = document.getElementById("close_container"); */
-  /* var menu = document.getElementById("menu"); */
-
-  /* var width = mms_logo.width + ext; */
-
-  /* side_nav.style.width = String(width) + "px"; */
-  /* main.style.marginLeft = String(width) + "px"; */
-
-  /* side_nav.style.background = "linear-gradient(to right, #f5f5f5 " + String(width - grad_offset) + "px, #c7c6c1)"; */
-  /* side_nav.style.paddingRight = String(grad_offset) + "px"; */
-
-  /* close_container.style.paddingRight = String(mult * grad_offset) + "px"; */
-  /* close_container.style.paddingTop = "10px"; */
-
-  /* menu.style.paddingRight = String(mult * grad_offset) + "px"; */
-
-  /* var height_mms_logo = parseInt(window.getComputedStyle(document.getElementById("mms_logo"), null).getPropertyValue("height")); */
-  /* var height_close_container = parseInt(window.getComputedStyle(document.getElementById("close_container"), null).getPropertyValue("height")); */
-  /* var height_menu = parseInt(window.getComputedStyle(document.getElementById("menu"), null).getPropertyValue("height")); */
-  /* var height_sidenav = parseInt(window.getComputedStyle(document.getElementById("sidenav"), null).getPropertyValue("height")); */
-
-  /* console.log(height_sidenav, height_mms_logo, height_close_container, height_menu); */
-
-  /* var padd = (height_sidenav - height_mms_logo - height_close_container) / 2 - height_menu; */
-
-  /* var padd = height_sidenav / 2 - height_menu - height_mms_logo - height_close_container; */
-
-  /* menu.style.paddingTop = String(padd) + "px"; */
-
